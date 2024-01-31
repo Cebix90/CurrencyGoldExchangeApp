7771931b4beae0887047c417cc68ce9bedfb337e
Cebix90 <cebulmat@gmail.com>
2024-02-01 00:37:35 +0100

    [Update] Updated Main class to use new functionalities from CurrencyExchangeService

--- src/main/java/org/currencygoldexchangeapp/Main.java
+++ src/main/java/org/currencygoldexchangeapp/Main.java
@@ -2,31 +2,43 @@ package org.currencygoldexchangeapp;
 
 import org.currencygoldexchangeapp.datamodels.CurrencyExchange;
 import org.currencygoldexchangeapp.handlers.ExchangeRateAPIHandler;
+import org.currencygoldexchangeapp.services.CurrencyExchangeService;
 
 import java.net.http.HttpClient;
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter currency code: ");
-            String currencyCode = scanner.next();
+            System.out.print("Enter source currency code: ");
+            String sourceCurrencyCode = scanner.next();
+
+            System.out.print("Enter target currency code (press ENTER if not applicable): ");
+            scanner.nextLine();
+            String targetCurrencyCode = scanner.nextLine();
+
+            System.out.print("Enter amount: ");
+            double amount = scanner.nextDouble();
+
+            // Konsumuj znak nowej linii, aby uniknąć problemu z pomiędzy nextDouble() a nextLine()
+            scanner.nextLine();
 
             System.out.print("Enter date (optional, press ENTER for today's date): ");
-            scanner.nextLine();
             String dateString = scanner.nextLine();
             LocalDate date = dateString.isEmpty() ? LocalDate.now() : LocalDate.parse(dateString);
 
             ExchangeRateAPIHandler exchangeRateAPIHandler = new ExchangeRateAPIHandler(HttpClient.newHttpClient());
-            CurrencyExchange exchangeRateResult = exchangeRateAPIHandler.getExchangeRateSingleCurrency(currencyCode, date.toString());
+            CurrencyExchangeService currencyExchangeService = new CurrencyExchangeService(exchangeRateAPIHandler);
 
-            System.out.println("Exchange rate for " + currencyCode + " on " + date + ": " + exchangeRateResult);
+            try {
+                CurrencyExchange result = currencyExchangeService.calculateExchangeAmount(sourceCurrencyCode, amount, targetCurrencyCode, date.toString());
+                System.out.println("Result of currency exchange: " + result);
+            } catch (Exception e) {
+                System.out.println("Error during currency exchange: " + e.getMessage());
+            }
 
-        } catch (DateTimeParseException e) {
-            System.out.println("Error during date parsing: " + e.getMessage());
         } catch (Exception e) {
             System.out.println("Another error: " + e.getMessage());
         }
