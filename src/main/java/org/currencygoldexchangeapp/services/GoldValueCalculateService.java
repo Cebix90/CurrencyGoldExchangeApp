0abc112efa05a35042d253722a06c61b33cb765d
Cebix90 <cebulmat@gmail.com>
2024-03-07 22:02:08 +0100

    [Update] changed type of parameter in calculateBestPrice method. Enhanced calculateGainOrLoss method to support date ranges using API data.

--- src/main/java/org/currencygoldexchangeapp/services/GoldValueCalculateService.java
+++ src/main/java/org/currencygoldexchangeapp/services/GoldValueCalculateService.java
@@ -4,6 +4,7 @@ import org.currencygoldexchangeapp.datamodels.GoldValue;
 import org.currencygoldexchangeapp.handlers.GoldValueAPIHandler;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 
@@ -14,13 +15,23 @@ public class GoldValueCalculateService {
         this.goldValueAPIHandler = goldValueAPIHandler;
     }
 
-    public BigDecimal calculateGainOrLoss(BigDecimal currentPrice, BigDecimal bestPrice) {
+    public BigDecimal calculateGainOrLoss(String startDate, String endDate) {
+        List<GoldValue> goldValueList = goldValueAPIHandler.getGoldValuesForDateRange(startDate, endDate);
+
+        List<BigDecimal> goldValueListConvertedToBigDecimal = goldValueList.stream()
+                .filter(d -> d.getEffectiveDate().equals(LocalDate.now().toString()))
+                .map(goldValue -> BigDecimal.valueOf(goldValue.getValue()))
+                .toList();
+
+        BigDecimal currentPrice = goldValueListConvertedToBigDecimal.getFirst();
+
+        BigDecimal bestPrice = calculateBestPrice(goldValueList);
+
         return currentPrice.subtract(bestPrice);
     }
 
-    public BigDecimal calculateBestPrice(int days) {
-        List<GoldValue> goldValues = goldValueAPIHandler.getLastGoldValues(days);
-        return goldValues.stream()
+    private BigDecimal calculateBestPrice(List<GoldValue> goldValueList) {
+        return goldValueList.stream()
                 .max(Comparator.comparing(GoldValue::getValue))
                 .map(goldValue -> BigDecimal.valueOf(goldValue.getValue()))
                 .orElseThrow(() -> new RuntimeException("No gold value data found in the response."));
