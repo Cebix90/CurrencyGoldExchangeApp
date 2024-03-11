884a232a252a1233e9eab8520f2a99725386b822
Cebix90 <cebulmat@gmail.com>
2024-03-11 18:34:46 +0100

    [Update] Added methods: getUserInputForCurrency and getDateForGold in InputUtility class. Whole logic responsible for user interactions has been moved to the UserMenuService class. The main class is responsible only for running the program.

--- src/main/java/org/currencygoldexchangeapp/utils/InputUtility.java
+++ src/main/java/org/currencygoldexchangeapp/utils/InputUtility.java
@@ -8,6 +8,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputUtility {
+    public static String getUserInputForCurrency(Scanner scanner, String message, String date, boolean isSourceCurrency) {
+        String currencyCode;
+        do {
+            System.out.print(message);
+            currencyCode = scanner.nextLine().toUpperCase();
+
+            if ("PLN".equalsIgnoreCase(currencyCode) && !isSourceCurrency) {
+                return currencyCode;
+            }
+        } while ((!currencyCode.isEmpty() && !InputUtility.isCurrencyAvailable(currencyCode, date)) || (isSourceCurrency && currencyCode.isEmpty()));
+        return currencyCode;
+    }
+
     public static boolean isCurrencyAvailable(String currency, String date) {
         List<String> codeCurrencyAvailableFor2011AndLater = Arrays.asList("USD", "AUD", "CAD", "EUR", "HUF", "CHF", "GBP", "JPY", "CZK", "DKK", "NOK", "SEK", "XDR");
         List<String> codeCurrencyAvailableFor2010AndBefore = Arrays.asList("USD", "AUD", "CAD", "EUR", "HUF", "CHF", "GBP", "JPY", "CZK", "DKK", "EEK", "NOK", "SEK", "XDR");
@@ -30,7 +43,7 @@ public class InputUtility {
                 .contains(currency.toLowerCase());
     }
 
-    public static LocalDate getDate(Scanner scanner) {
+    public static LocalDate getDateForCurrencies(Scanner scanner) {
         LocalDate date = null;
         LocalDate today = LocalDate.now();
 
@@ -44,13 +57,44 @@ public class InputUtility {
                 } else {
                     date = LocalDate.parse(dateString);
 
-                    LocalDate minDate = LocalDate.of(2003, 1, 1);
+                    LocalDate minDate = LocalDate.of(2003, 1, 2);
 
                     if (date.isAfter(today)) {
-                        System.out.println("Entered date is greater than today's date. Please enter a date within the range from 2003-01-01 to " + today + ".");
+                        System.out.println("Entered date is greater than today's date. Please enter a date within the range from 2003-01-02 to " + today + ".");
                         date = null;
                     } else if (date.isBefore(minDate)) {
-                        System.out.println("Entered date is earlier than the supported date range. Please enter a date within the range from 2003-01-01 to " + today + ".");
+                        System.out.println("Entered date is earlier than the supported date range. Please enter a date within the range from 2003-01-02 to " + today + ".");
+                        date = null;
+                    }
+                }
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid date format. Please enter a valid date in the format yyyy-MM-dd");
+            }
+        }
+        return date;
+    }
+
+    public static LocalDate getDateForGold(Scanner scanner) {
+        LocalDate date = null;
+        LocalDate today = LocalDate.now();
+
+        while (date == null) {
+            System.out.print("Please enter a date within the range from 2013-01-02 to " + today + " (optional, press ENTER for today's date): ");
+            String dateString = scanner.nextLine();
+
+            try {
+                if (dateString.isEmpty()) {
+                    date = LocalDate.now();
+                } else {
+                    date = LocalDate.parse(dateString);
+
+                    LocalDate minDate = LocalDate.of(2013, 1, 2);
+
+                    if (date.isAfter(today)) {
+                        System.out.println("Entered date is greater than today's date. Please enter a date within the range from 2013-01-02 to " + today + ".");
+                        date = null;
+                    } else if (date.isBefore(minDate)) {
+                        System.out.println("Entered date is earlier than the supported date range. Please enter a date within the range from 2013-01-02 to " + today + ".");
                         date = null;
                     }
                 }
